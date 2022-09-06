dee9876618880c7a46b2fd1d4edc60be6a99fcb8
sanoo <sanookumar17@gmail.com>
2022-09-05 20:21:40 -0500

    updated data file and data html page

--- Resources/app.py
+++ Resources/app.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-df = pd.read_csv('./cities.csv')
+df = pd.read_csv('./cities.csv', usecols=['City','Lat','Lng','Max Temp','Humidity','Cloudiness','Wind Speed','Country','Date'])
 df.to_html('./cities.html')
